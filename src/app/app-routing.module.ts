bec69c10444b94467bd3045d5ac9ab081b9ea8bc
Robert <dumbrave.robert@gmail.com>
2019-11-28 18:41:16 +0200

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },  { path: 'page', loadChildren: './tab1/page/page.module#PagePageModule' }
+
 ];
 @NgModule({
   imports: [
