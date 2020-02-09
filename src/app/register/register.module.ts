127f8996ffdb38704bc58d47bbbb1b8265a962f1
Robert <dumbrave.robert@gmail.com>
2020-02-09 18:12:03 +0200

    merge

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -6,8 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { RegisterPage } from './register.page';
-import * as firebase from 'firebase';
-import { environment } from 'src/environments/environment';
+
 
 const routes: Routes = [
   {
